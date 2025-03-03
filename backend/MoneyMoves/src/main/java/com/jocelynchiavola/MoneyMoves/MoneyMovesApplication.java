cb1070cad4193d57b6b763dbad064eece818dedb
jmchiavola25 <jmchiavola25@vt.edu>
2025-03-03 14:27:57 -0500

    Set up backend with Java Springboot Gradle

---
+++ backend/MoneyMoves/src/main/java/com/jocelynchiavola/MoneyMoves/MoneyMovesApplication.java
@@ -0,0 +1,15 @@
+package com.jocelynchiavola.MoneyMoves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMovesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMovesApplication.class, args);
+
+		System.out.println("Started application");
+	}
+
+}
---
+++ backend/MoneyMoves/src/test/java/com/jocelynchiavola/MoneyMoves/MoneyMovesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jocelynchiavola.MoneyMoves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMovesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
