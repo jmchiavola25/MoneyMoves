d3361548428ad43fa0ea56893e6c5af45163a464
jmchiavola25 <jmchiavola25@vt.edu>
2025-03-21 15:05:17 -0400

    Add chakra provider to app

--- frontend/money-moves/src/pages/App.tsx
+++ frontend/money-moves/src/pages/App.tsx
@@ -5,17 +5,20 @@ import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 import Dashboard from "../pages/Dashboard";
 import "../styles/App.css"
+import { Provider } from '../components/ui/provider';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
